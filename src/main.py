8a585324c8d3585101e6079a4021d5518649d710
rendoir <danfil.marques@gmail.com>
2018-03-01 21:34:05 +0000

    Loaded data into data structure

--- src/main.py
+++ src/main.py
@@ -1,26 +1,33 @@
-import sys
+#import mnist_loader
+#training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+'''
+print(type(training_data))
+for test_3 in training_data:
+    print(type(test_3))
+    print(type(test_3[0]))
+    print(type(test_3[1]))
+    print(test_3[0].shape)
+    print(test_3[1].shape)
+'''    
+#import network
+#net = network.Network([784, 10, 10])
+#net.SGD(training_data, 10, 10, 3.0, test_data=test_data)
 
-'''
-import mnist_loader
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-'''
 
 import numpy
-filename = '../HTRU2/HTRU_2.csv'
+filename = '../HTRU2/test.csv'
 raw_data = open(filename, 'rU')
 data = numpy.loadtxt(raw_data, delimiter=',')
-
-#debug
-print(type(data))
-print(data.shape)
-for row in data: 
-    for element in row:
-        sys.stdout.write(str(element))
-        sys.stdout.write(', ')
-    sys.stdout.write('\n')
-
-'''
+training_data = list()
+for test in data:
+    training_data.append((test[:-1].reshape(8, 1), test[-1].reshape(1, 1)))
+print(type(training_data))
+for tuple in training_data:
+    print(type(tuple))
+    print(type(tuple[0]))
+    print(tuple[0].shape)
+    print(type(tuple[1]))
+    print(tuple[1].shape)
 import network
-net = network.Network([784, 10, 10])
-net.SGD(training_data, 10, 10, 3.0, test_data=test_data)
-'''
+net = network.Network([8, 5, 1])
+net.SGD(training_data, 2, 2, 3.0, test_data=training_data)
